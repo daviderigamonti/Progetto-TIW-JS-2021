2d4596576dc1c61dd5542a2c82ba87a98bf6e377
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2021-06-09 17:35:30 +0200

    Added filters package, HTML and JS files

---
+++ Progetto-TIW-JS-2021/WebContent/js/login.js
@@ -0,0 +1,11 @@
+/**
+ * Login
+ */
+
+(function() {	// Nasconde le variabli dallo scope globale
+
+	document.getElementById("bottoneLogin").addEventListener("click", (event) => {
+		//FUNZIONE DI LOGIN
+	})
+	
+})();
